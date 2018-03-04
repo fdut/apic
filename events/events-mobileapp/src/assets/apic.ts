8001d334a4a2a328a2c077a75a7296085764533b
fred <fdutheil@laposte.net>
2018-03-04 16:38:09 +0100

    First commit

--- events/events-mobileapp/src/assets/apic.ts
+++ events/events-mobileapp/src/assets/apic.ts
@@ -1,7 +1,7 @@
 export default 
     {
-      clientid: 'REPLACE_WITH_YOUR_CLIENTID',
-      clientsecret: 'REPLACE_WITH_YOUR_CLIENTSECRET',
-      apicEndpoint: 'REPLACE_WITH_YOUR_APICENDPOINT'
+      clientid: 'fc3978fd-6d26-4e85-88af-021bbeafe5b8',
+      clientsecret: 'cC3xP5bL8iC0iF3lR2dP5pN3fK5kW7vK1wD1lI8tR4fS3eT1aB',
+      apicEndpoint: 'https://api.eu-de.apiconnect.ibmcloud.com/fdutorg-dev/sb'
     };
   
